31531a11ba31e1b66bab00c0f12fc9ead3dbc5f1
Cristiano <cristiano.molin@luminiitsolutions.com>
2021-01-13 15:15:35 -0300

    Payroll-MS

--- ms-user/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tb_user(name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$zobO249hrbMBACHyAf5AiOzlMS0WXNIA6s1RpGpfGttpO0V3W4t46');
-INSERT INTO tb_user(name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$zobO249hrbMBACHyAf5AiOzlMS0WXNIA6s1RpGpfGttpO0V3W4t46');
-
-INSERT INTO tb_role(role_name) VALUES ('ROLE_OPERATOR');
-INSERT INTO tb_role(role_name) VALUES ('ROLE_ADMIN');
-
-INSERT INTO tb_user_role(user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (2, 2);
